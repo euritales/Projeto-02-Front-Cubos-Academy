c7a973e7ffb121eaad3e84a0e5a2304aabc81717
euritales <58851676+euritales@users.noreply.github.com>
2021-06-23 19:47:56 -0300

    novos filmes

--- desafio-frontend-modulo2-noturno/desafio2/src/data.js
+++ desafio-frontend-modulo2-noturno/desafio2/src/data.js
@@ -1,5 +1,5 @@
 const BASE_PATH = 'https://image.tmdb.org/t/p/original/';
-
+const BASE_PATH2 = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/';
 const Movies = [
   {
     title: 'Mortal Kombat',
@@ -44,45 +44,45 @@ const Movies = [
     price: 6.5
   },
   {
-    title: 'Mortal Kombat',
-    poster_path: `${BASE_PATH}w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg`,
-    vote_average: 9,
-    price: 7.5
-  },
-  {
-    title: 'Godzilla vs. Kong',
-    poster_path: `${BASE_PATH}klAIFewuqcyEmH1SMtR2XbMyqzM.jpg`,
-    vote_average: 8.5,
-    price: 3
-  },
-  {
-    title: 'Anônimo',
-    poster_path: `${BASE_PATH}woPYFGKwVULILbO7q44HmsNY0kO.jpg`,
-    vote_average: 8.5,
+    title: 'Cruella',
+    poster_path: `${BASE_PATH2}ljPHd7WiPVKmuXi1hgQUpZQslbC.jpg`,
+    vote_average: 8.6,
     price: 7
   },
   {
-    title: 'Sem Remorso',
-    poster_path: `${BASE_PATH}uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg`,
-    vote_average: 8.4,
+    title: 'Um Lugar Silencioso - Parte II',
+    poster_path: `${BASE_PATH2}wYqmuxd9jNFLf3gtQY5vbAUgiGk.jpg`,
+    vote_average: 7.3,
+    price: 4
+  },
+  {
+    title: 'Invocação do Mal 3: A Ordem do Demônio',
+    poster_path: `${BASE_PATH2}8jBgDFFDYz8WHxiOBJ7GBiMM6Pe.jpg`,
+    vote_average: 8.1,
     price: 5.5
   },
   {
-    title: 'Demon Slayer: O Trem Infinito',
-    poster_path: `${BASE_PATH}m9cn5mhW519QKr1YGpGxNWi98VJ.jpg`,
-    vote_average: 8.4,
-    price: 8
+    title: 'Luca',
+    poster_path: `${BASE_PATH2}1lk7QISmTam2iOzzUAQYs6voeFg.jpg`,
+    vote_average: 8.3,
+    price: 6.5
   },
   {
-    title: 'Cherry: Inocência Perdida',
-    poster_path: `${BASE_PATH}qbSsR0IfnxdXZqfRMpH2PgLAbPV.jpg`,
-    vote_average: 8.1,
-    price: 8.5
+    title: 'Velozes & Furiosos 9',
+    poster_path: `${BASE_PATH2}8L4Mep3KDUK4ztUgf2HlPvUVzZy.jpg`,
+    vote_average: 8.0,
+    price: 5
   },
   {
-    title: 'Benny Loves You',
-    poster_path: `${BASE_PATH}mQ8vALvqA0z0qglG3gI6xpVcslo.jpg`,
-    vote_average: 7.7,
+    title: 'Pedro Coelho 2',
+    poster_path: `${BASE_PATH2}pxdSJCpqMOp3gakScrzHgGDEvVr.jpg`,
+    vote_average: 7.1,
+    price: 6
+  },
+  {
+    title: 'Infiltrado',
+    poster_path: `${BASE_PATH2}ef2ozkE4AiWwRnyfc9s05sEtAIT.jpg`,
+    vote_average: 7.8,
     price: 6.5
   },
 ]
